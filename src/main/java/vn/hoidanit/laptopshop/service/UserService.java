9218f69d7e6d620903d4320bb0fcc51895bc1845
BangLV <wuubangdev@gmail.com>
2024-03-27 11:03:03 +0700

    video 46

---
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -0,0 +1,11 @@
+package vn.hoidanit.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String handleHello() {
+        return "hello form service";
+    }
+}
