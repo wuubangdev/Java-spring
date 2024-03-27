05d290370f4928bffc8b2af614a40466e177d999
BangLV <wuubangdev@gmail.com>
2024-03-27 09:57:29 +0700

    video 43

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -5,9 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String index() {
-        return "Hello World with BITV!";
+        return "Hello World with update!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!";
     }
 
 }
